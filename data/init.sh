ef4da0f324a8040d79ef2523d1552ce8a60b9360
Matěj Pavlíček <tikinang@gmail.com>
2024-11-19 17:08:32 +0100

    add readiness check, fix psqls

--- data/init.sh
+++ data/init.sh
@@ -2,13 +2,15 @@
 
 set -u
 
-psql -h postgis -p 5432 -U postgres -c "CREATE EXTENSION postgis;"
-psql -h postgis -p 5432 -U postgres -c "ALTER user postgis CREATEDB;"
+psql -h postgis.zerops -c "CREATE EXTENSION postgis;" || true
+psql -h postgis.zerops -c "CREATE DATABASE bonn;"
 
-export PGPASSWORD="$DB_PASSWORD"
-psql -h postgis -p 5432 -U postgis -c "CREATE DATABASE bonn;"
-psql -h postgis -p 5432 -U postgis -d bonn < ./data/bonn_example.sql
-psql -h postgis -p 5432 -U postgis -d bonn < ./data/sql/add_index.sql
-
+psql -h postgis.zerops -d bonn < ./data/bonn_example.sql
+psql -h postgis.zerops -d bonn < ./data/sql/add_index.sql
 rm ./data/bonn_example.sql
 rm ./data/._bonn_example.sql
+
+psql -h postgis.zerops -c "ALTER DATABASE bonn OWNER TO postgis;"
+psql -h postgis.zerops -d bonn -c "ALTER SCHEMA public OWNER TO postgis;"
+psql -h postgis.zerops -d bonn -c "GRANT ALL ON DATABASE bonn TO postgis;"
+psql -h postgis.zerops -d bonn -c "GRANT ALL ON ALL TABLES IN SCHEMA public TO postgis;"
