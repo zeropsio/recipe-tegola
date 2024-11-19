df927f33f9290ea7d4771c25c6247b92e7a0ddf6
Matěj Pavlíček <tikinang@gmail.com>
2024-11-19 16:28:51 +0100

    feat: zeropsify

---
+++ README.md
@@ -0,0 +1,5 @@
+# Zerops x Tegola
+
+[Tegola](https://github.com/go-spatial/tegola) is a vector tile server delivering Mapbox Vector Tiles with support for PostGIS, GeoPackage and SAP HANA Spatial data providers. User documentation can be found at [tegola.io](https://tegola.io/).
+
+This recipe is a showcase of Tegola demo project with city of Bonn, deployed with one-click to [Zerops](https://zerops.io). No need for Docker. Enjoy!
--- data/init.sh
+++ data/init.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-sleep 3
-psql -h postgis -p 5432 -U postgres -d bonn < ./data/bonn_example.sql
-psql -h postgis -p 5432 -U postgres -d bonn -f ./data/sql/add_index.sql
+set -u
+
+psql -h postgis -p 5432 -U postgres -c "CREATE EXTENSION postgis;"
+psql -h postgis -p 5432 -U postgres -c "ALTER user postgis CREATEDB;"
+
+export PGPASSWORD="$DB_PASSWORD"
+psql -h postgis -p 5432 -U postgis -c "CREATE DATABASE bonn;"
+psql -h postgis -p 5432 -U postgis -d bonn < ./data/bonn_example.sql
+psql -h postgis -p 5432 -U postgis -d bonn < ./data/sql/add_index.sql
+
 rm ./data/bonn_example.sql
 rm ./data/._bonn_example.sql
